aa081b4b3726e9567979cecfa09c3225e5ef4409
DanielSCF <needdaniel@hotmail.com>
2022-05-23 17:16:45 -0500

    Completed brand crud

--- src/main/java/com/eduale/bodega/controlador/MarcaControlador.java
+++ src/main/java/com/eduale/bodega/controlador/MarcaControlador.java
@@ -3,8 +3,10 @@ package com.eduale.bodega.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +26,18 @@ public class MarcaControlador {
 		return marcaService.listarMarca();
 	}
 	 
-	@PostMapping()
+	@PostMapping
 	public Marca guardarMarca(@RequestBody Marca t) {
 		return marcaService.guardarMarca(t);
+	}
+
+	@PutMapping
+	public Marca editarMarca(@RequestBody Marca t) {
+		return marcaService.editarMarca(t);
+	}
+	
+	@DeleteMapping
+	public void eliminarMarca(@RequestBody Marca t) {
+		marcaService.eliminarMarca(t);
 	}
 }
--- src/main/java/com/eduale/bodega/servicio/MarcaServicio.java
+++ src/main/java/com/eduale/bodega/servicio/MarcaServicio.java
@@ -8,19 +8,27 @@ import org.springframework.stereotype.Service;
 import com.eduale.bodega.modelo.Marca;
 import com.eduale.bodega.repositorio.IMarca;
 
-
-
 @Service
 public class MarcaServicio {
 	@Autowired
 	private IMarca datamarca;
-	
-	public List<Marca> listarMarca(){
+
+	public List<Marca> listarMarca() {
 		return (List<Marca>) datamarca.findAll();
 	}
-	
+
 	public Marca guardarMarca(Marca u) {
 		return datamarca.save(u);
-		
 	}
+
+	public Marca editarMarca(Marca u) {
+		return datamarca.save(u);
+	}
+
+	public void eliminarMarca(Marca u){
+		datamarca.delete(u);
+	} 
+
+
 }
+
